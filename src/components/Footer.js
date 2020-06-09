6a6235dd34a7df29edca109fff8ab64d85e1c9d8
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-06-09 19:50:17 +0100

    footr update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,23 +3,45 @@ import React, { Component } from 'react'
 class Footer extends Component {
     render() {
         return (
-          <div style={footerStyle}>
-            <h1
-            >
-              Footer content
-            </h1>
+          <div>
+            <div style={footerColor}>
+              <div>
+                <p>10000000</p>
+              </div>
+              <div style={footerStyle}>
+                <p> About</p>
+                <p> Privacy Policy </p>
+                <p> Terms of Service </p>
+              </div>
+            </div>
           </div>
         );
     }
 }
 
 //Styling
+const footerColor = {
+  position: 'fixed',
+  bottom: '-15%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  minWidth: '100%',
+  background: 'blue',
+  height: '100px',
+};
+
 const footerStyle = {
   position: 'fixed',
-  bottom: '0px',
-  left: '50%',
-  top: '50%',
-  transform: 'translate(50%, 50%)',
+  bottom: '-15%',
+  left: '20%',
+  transform: 'translate(-50%, -50%)',
+  minWidth: '80%',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  paddingLeft: '10px',
+  paddingRight: '10px',
+  height: '100px',
 };
 
 // PropTypes
