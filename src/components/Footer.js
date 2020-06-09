0bcbc13597d15c2e61eac8d8414c17699d374053
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-06-09 23:04:25 +0100

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './compo'
 
 class Footer extends Component {
     render() {
         return (
           <div>
             <div style={footerColor}>
-              <div>
+              <div style={footerStyleLeft}>
+                <p>10000000</p>
+                <p>10000000</p>
                 <p>10000000</p>
               </div>
               <div style={footerStyle}>
@@ -28,21 +31,40 @@ const footerColor = {
   minWidth: '100%',
   background: 'blue',
   height: '100px',
+  
 };
 
-const footerStyle = {
+const footerStyleLeft = {
   position: 'fixed',
-  bottom: '-15%',
+  bottom: '-50%',
   left: '20%',
   transform: 'translate(-50%, -50%)',
-  minWidth: '80%',
+  minWidth: '40%',
   display: 'flex',
   flexDirection: 'row',
   justifyContent: 'space-between',
   paddingLeft: '10px',
   paddingRight: '10px',
   height: '100px',
+  color: 'white'
 };
+
+
+const footerStyle = {
+  position: 'fixed',
+  bottom: '-50%',
+  right: '-20%',
+  transform: 'translate(-50%, -50%)',
+  minWidth: '40%',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  paddingLeft: '10px',
+  paddingRight: '10px',
+  height: '100px',
+  color: 'white'
+};
+
 
 // PropTypes
 Footer.propType = {
